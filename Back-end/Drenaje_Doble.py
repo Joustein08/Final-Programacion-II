3fd781fe3ff01524869ff223d3bbe3a2eec73e1d
Joustein08 <jodperezc@udistrital.edu.co>
2023-05-03 09:31:05 -0500

    Update Drenaje_Doble.py

--- Back-end/Drenaje_Doble.py
+++ Back-end/Drenaje_Doble.py
@@ -3,5 +3,5 @@ import geopandas as gpd
 import matplotlib as plt
 
 # Importación información de los rios
-rios = gpd.read_file("D:\Documentos\Jous\Ing-ud\Materias\2023-1\Programación\Final\Final-Programacion-II\Datos\Drenaje_doble_83IIIB.zip")
+rios = gpd.read_file("D:/Documentos/Jous/Ing-ud/Materias/2023-1/Programación/Final/Final-Programacion-II/Datos/Drenaje_doble_83IIIB.zip")
 rios.head(5)
