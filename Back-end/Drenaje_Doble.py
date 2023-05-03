49483eef58904b868ac7ade38332bb0ab2348306
Joustein08 <jodperezc@udistrital.edu.co>
2023-05-03 15:20:26 -0500

    Update Drenaje_Doble.py

--- Back-end/Drenaje_Doble.py
+++ Back-end/Drenaje_Doble.py
@@ -5,24 +5,22 @@ import matplotlib as plt
 # Importación información de los rios
 rios = gpd.read_file("D:/Documentos/Jous/Ing-ud/Materias/2023-1/Programación/Final/Final-Programacion-II/Datos/Drenaje_doble_83IIIB.zip")
 
-rios["PROCESO"]="UNION"
-rios_unidos = rios.dissolve('PROCESO')
-rios_unidos["buffer1"] = rios_unidos.buffer(500)
-rios_unidos["Categoria_1"] = (rios_unidos["buffer1"]-rios_unidos["geometry"])
-rios_unidos["buffer2"] = rios_unidos.buffer(1000)
-rios_unidos["Categoria_2"] = (rios_unidos["buffer2"]-rios_unidos["Categoria_1"]-rios_unidos["geometry"])
-rios_unidos["buffer3"] = rios_unidos.buffer(4000)
-rios_unidos["Categoria_3"] = (rios_unidos["buffer3"]-rios_unidos["Categoria_2"]-rios_unidos["Categoria_1"]-rios_unidos["geometry"])
-limites = rios_unidos.total_bounds
+#Creación de columnas en el geodataframe para el análisis
+rios["PROCESO"]="UNION" # Creación de columna proceso para realizar la union de los elementos del archivo shp
+rios_unidos = rios.dissolve('PROCESO') # Union de los elementos del archivo shp
+rios_unidos["buffer1"] = rios_unidos.buffer(500) # Creación primera zona
+rios_unidos["Categoria_1"] = (rios_unidos["buffer1"]-rios_unidos["geometry"]) # Delimitación primera zona
+rios_unidos["buffer2"] = rios_unidos.buffer(1000) # Creación segunda zona
+rios_unidos["Categoria_2"] = (rios_unidos["buffer2"]-rios_unidos["Categoria_1"]-rios_unidos["geometry"]) # Delimitación segunda zona
+rios_unidos["buffer3"] = rios_unidos.buffer(4000) # Creación tercera zona
+rios_unidos["Categoria_3"] = (rios_unidos["buffer3"]-rios_unidos["Categoria_2"]-rios_unidos["Categoria_1"]-rios_unidos["geometry"]) # Delimitación tercera zona
 
-# Grafica
-categorias = rios_unidos["geometry"].plot(figsize = (12,12), alpha=0.5, color = "b")
-rios_unidos["Categoria_1"].plot(ax = categorias, color = "r")
-rios_unidos["Categoria_2"].plot(ax = categorias, color = "y")
-rios_unidos["Categoria_3"].plot(ax = categorias, color = "g")
+# Gráfica
+categorias = rios_unidos["geometry"].plot(figsize = (12,12), alpha=0.5, color = "b") # Área del rio
+rios_unidos["Categoria_1"].plot(ax = categorias, color = "r") # Área de la primera categoria
+rios_unidos["Categoria_2"].plot(ax = categorias, color = "y") # Área de la segunda categoria
+rios_unidos["Categoria_3"].plot(ax = categorias, color = "g") # Área de la tercera categoria
 
-
-#rios.head(8)
-
-categorias.set_xlim(limites[0],limites[2])
-categorias.set_ylim(limites[1],limites[3])
+# Delimitación gráfica
+categorias.set_xlim(rios_unidos.total_bounds[0],rios_unidos.total_bounds[2]) # Eje x
+categorias.set_ylim(rios_unidos.total_bounds[1],rios_unidos.total_bounds[3]) # Eje y
