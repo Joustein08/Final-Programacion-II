e2802818db66763dee191020072ae38049128ffb
Joustein08 <jodperezc@udistrital.edu.co>
2023-05-03 12:09:33 -0500

    Update Drenaje_Doble.py

--- Back-end/Drenaje_Doble.py
+++ Back-end/Drenaje_Doble.py
@@ -4,8 +4,15 @@ import matplotlib as plt
 
 # Importación información de los rios
 rios = gpd.read_file("D:/Documentos/Jous/Ing-ud/Materias/2023-1/Programación/Final/Final-Programacion-II/Datos/Drenaje_doble_83IIIB.zip")
+rios["buffer1"] = rios.buffer(200)
+rios["Categoria_1"] = (rios["buffer1"]-rios["geometry"])
+rios["buffer2"] = rios.buffer(400)
+rios["Categoria_2"] = (rios["buffer2"]-rios["Categoria_1"]-rios["geometry"])
 
-rios["buffer"] = rios.buffer(50)
-rios["buffer"].plot(figsize = (12,12))
+# Grafica
+categorias = rios["geometry"].plot(figsize = (12,12), alpha=0.5, color = "b")
+rios["Categoria_1"].plot(ax = categorias, color = "r")
+rios["Categoria_2"].plot(ax = categorias, color = "y")
+rios.head(8)
 
 # hola
