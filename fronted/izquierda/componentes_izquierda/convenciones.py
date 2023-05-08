60bb67035305286ae3a5dbe7ef92a74d79450d9c
CarlosPrietoL <caaprietol@udistrital.edu.co>
2023-05-08 11:27:00 -0500

    Convenciones :+1:

--- fronted/izquierda/componentes_izquierda/convenciones.py
+++ fronted/izquierda/componentes_izquierda/convenciones.py
@@ -1,9 +1,22 @@
 from dash import html
 import dash_bootstrap_components as dbc
 
+#Creacion container "CONVENCIONES"
 convenciones = dbc.Container(
     [
         html.H2('CONVENCIONES'),
         html.Hr(),
+        #Creacion tabla
+        dbc.Table(
+            [
+                html.Tr([
+                    # Creacion celdas de la tabla
+                    html.Td('MALO', style={'background-color': 'red', 'fontWeight': 'bold'}),
+                    html.Td('REGULAR', style={'background-color': 'yellow', 'fontWeight': 'bold'}),
+                    html.Td('BUENO', style={'background-color': 'green', 'fontWeight': 'bold'}),
+                ])
+            ],
+            style={'width': '100%'} #Hace que la tabla ocupe todo el ancho del container
+        )
     ]
 )
--- fronted/izquierda/componentes_izquierda/datos_a_analizar.py
+++ fronted/izquierda/componentes_izquierda/datos_a_analizar.py
@@ -1,14 +1,35 @@
 from dash import html
+from dash import dcc
 import dash_bootstrap_components as dbc
 
+
+#Creación container para la parte izquierda.
 datos_a_analizar = dbc.Container(
     [
         html.H2('DATOS A ANALIZAR'),
         html.Hr(),
+        #Creacion de label + boton carga para archivos .zip
         html.Label('Centros Poblados'),
-        dbc.Input(value='Población.shp'),
+        dcc.Upload(
+            id='upload-data',
+            children=html.Div([
+                'Arrastre y suelte el archivo o ',
+                html.A('Seleccione el archivo')
+            ]),
+            style={
+                'width': '100%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px'
+             },
+             # Acepta archivos .zip
+             accept='.zip'
+         ),
         html.Br(),
-        dbc.Button('Subir', color='primary', className='mr-2'),
         html.Hr(),
         html.Label('Cartografía'),
         dbc.Input(value='Cartografía.shp'),
