1cc054fc5e35af3d9dd285bf1fe4a6178ad5aea8
CarlosPrietoL <caaprietol@udistrital.edu.co>
2023-05-08 09:45:46 -0500

    .

--- fronted/derecha/componentes_derecha/grafica_final.py
+++ fronted/derecha/componentes_derecha/grafica_final.py
@@ -5,7 +5,7 @@ grafica_final = dbc.Container(
     [
         html.H2('GRÁFICA FINAL'),
         html.Hr(),
-        html.Img(src='C:/Users/usuario/Desktop/Proyecto programación II/Imagenes/Ubicacion.png', style={'width': '50%'}),
+        html.Img(src='./ubicacion.png'),
         html.Br(),
         dbc.Button('Descargar', color='primary', className='mr-2'),
         html.Hr(),
