d70e6d615f5c720695fcedb588ca62772e4b12c8
Joustein08 <jodperezc@udistrital.edu.co>
2023-05-09 12:39:57 -0500

    Update definicion_proyecto.py

--- fronted/izquierda/componentes_izquierda/definicion_proyecto.py
+++ fronted/izquierda/componentes_izquierda/definicion_proyecto.py
@@ -1,9 +1,14 @@
-from dash import html
+from dash import html, dcc
 import dash_bootstrap_components as dbc
 
 definicion_proyecto = dbc.Container(
     [
         html.H2('DEFINICIÓN PROYECTO'),
         html.Hr(),
+        html.Label('¿Al proyecto le afecta la proximidad con construcciones similares'),
+        dcc.Dropdown(
+            id = 'proximidadConstrucciones',
+            options = ['si', 'no'],
+        ),
     ]
 )
