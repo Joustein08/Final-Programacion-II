5ce802774a285a816184e7b6aa859c5ad8313163
Joustein08 <jodperezc@udistrital.edu.co>
2023-05-09 12:05:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import dash
 from dash import html
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 
 #IMPORTAR FRONTED
 from fronted.navegador.navegador import navegador
 from fronted.derecha.derecha import derecha
 from fronted.izquierda.izquierda import izquierda
+
+#IMPORTAR BACKEND
+from Backend.Construcciones import *
 
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -20,8 +24,14 @@ app.layout = dbc.Container(
         )
     ]
 )
-
-
+@app.callback(
+    Output('salidaConstrucciones', 'children'),
+    Input('proximidad', 'value')
+)
+def construcciones(proximidad):
+    construcciones_codificada = analisisConstrucciones(proximidad)
+    imagenConstrucciones = html.Img(src="data:image/png;base64,{}".format(construcciones_codificada))
+    return html.Div([imagenConstrucciones])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
