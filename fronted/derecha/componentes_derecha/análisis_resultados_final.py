fa7e526a9ace2dd583a87019052bf4888ec2643a
Joustein08 <jodperezc@udistrital.edu.co>
2023-05-13 06:57:25 -0500

    Update análisis_resultados_final.py

--- fronted/derecha/componentes_derecha/análisis_resultados_final.py
+++ fronted/derecha/componentes_derecha/análisis_resultados_final.py
@@ -7,6 +7,8 @@ analisis_resultados_final = dbc.Container(
         html.Hr(),
         html.H5('Según el análisis generado por el software se especifica:'),
         html.Hr(),
+        html.Label('Análisis de construcciones similares'),
+        html.Hr(),
         html.Div(id = 'salidaConstrucciones')
     ]
 )
