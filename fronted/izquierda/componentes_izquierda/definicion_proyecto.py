2e48461e61efbdb075e783b7c91a8f1fce0661f4
CarlosPrietoL <caaprietol@udistrital.edu.co>
2023-05-15 00:07:13 -0500

    avance :8ball:

--- fronted/izquierda/componentes_izquierda/definicion_proyecto.py
+++ fronted/izquierda/componentes_izquierda/definicion_proyecto.py
@@ -10,9 +10,22 @@ definicion_proyecto = dbc.Container(
             id = 'proximidadConstrucciones',
             options = ['SI', 'NO'],
         ),
+        html.Br(),
         html.Label('¿Al proyecto le afecta la proximidad con drenajes, rios o cuerpos de agua?'),
         dcc.Dropdown(
             id = 'proximidadRios',
+            options = ['SI', 'NO'],
+        ),
+        html.Br(),
+        html.Label('¿Al proyecto le afecta la proximidad con centros poblados?'),
+        dcc.Dropdown(
+            id = 'proximidadPoblación',
+            options = ['SI', 'NO'],
+        ),
+        html.Br(),
+        html.Label('¿Es de interés para el proyecto al análisis de red vial?'),
+        dcc.Dropdown(
+            id = 'proximidadVias',
             options = ['SI', 'NO'],
         ),
     ]
