0fb41ab30ec4e833ad0baebc657c9f8a2e457ed9
Joustein08 <jodperezc@udistrital.edu.co>
2023-05-17 13:54:27 -0500

    Update grafica_final.py

--- fronted/derecha/componentes_derecha/grafica_final.py
+++ fronted/derecha/componentes_derecha/grafica_final.py
@@ -5,7 +5,7 @@ grafica_final = dbc.Container(
     [
         html.H2('GRÁFICA FINAL'),
         html.Hr(),
-        html.Img(src='C:\\Users\\usuario\\Documents\\GitHub\\Final-Programacion-II\\fronted\\derecha\\componentes_derecha\\Ubicacion.png', style={'width': '500px', 'height': '300px'}),
+        html.Img(src='D:/Documentos/Jous/Ing-ud/Materias/2023-1/Programación/Final/Final-Programacion-II/fronted/derecha/componentes_derecha/Ubicacion.png', style={'width': '500px', 'height': '300px'}),
         html.Br(),
         dbc.Button('Descargar', color='primary', className='mr-2'),
         html.Hr(),
