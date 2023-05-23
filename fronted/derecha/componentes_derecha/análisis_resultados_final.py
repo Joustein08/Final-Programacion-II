b7d58c0d6f9a291666b60e8c893b2834a8776a2d
Joustein08 <jodperezc@udistrital.edu.co>
2023-05-23 13:58:28 -0500

    Update análisis_resultados_final.py

--- fronted/derecha/componentes_derecha/análisis_resultados_final.py
+++ fronted/derecha/componentes_derecha/análisis_resultados_final.py
@@ -2,23 +2,23 @@ from dash import html, dcc
 import dash_bootstrap_components as dbc
 
 analisis_resultados_final = dbc.Container(
-    [
-        html.H2('ANÁLISIS DE RESULTADOS FINAL'),
-        html.Hr(),
-        html.H5('Según el análisis generado por el software se especifica:'),
-        html.Hr(),
-        html.Label('Análisis de construcciones similares'),
-        html.Hr(),
-        html.Div(id = 'salidaConstrucciones'),
-        html.Hr(),
-        html.Label('Análisis de cuerpos de agua'),
-        html.Hr(),
-        html.Div(id = 'salidaRios'),
-        html.Label('Análisis de cuerpos de población'),
-        html.Hr(),
-        html.Div(id = 'salidaPoblacion'),
-        # html.Label('Análisis de a cuerdo a la red vial'),
-        # html.Hr(),
-        # html.Div(id = 'salidaVias'),
-    ]
-)
+     [         
+         html.H2('ANÁLISIS DE RESULTADOS FINAL'),
+         html.Hr(),
+         html.H5('Según el análisis generado por el software se especifica:'),
+         html.Hr(),
+         html.Label('Análisis de construcciones similares'),
+         html.Hr(),
+         html.Div(id = 'salidaConstrucciones'),
+        #  html.Hr(),
+        #  html.Label('Análisis de cuerpos de agua'),
+        #  html.Hr(),
+        #  html.Div(id = 'salidaRios'),
+        #  html.Label('Análisis de cercanía de población'),
+        #  html.Hr(),
+        #  html.Div(id = 'salidaPoblacion'),
+         # html.Label('Análisis de a cuerdo a la red vial'),
+         # html.Hr(),
+         # html.Div(id = 'salidaVias'),
+     ]
+ )
